3a9fa8815cd794826c6e588fddac8fc0049d8767
MikhailDzhimilyuk <mdzhimilyuk@mail.com>
2024-12-20 16:46:26 +0300

    changed seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,32 +1,34 @@
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient()
 async function main() {
+  const hashedPassword = await bcrypt.hash("12345", 5);
   const alice = await prisma.user.upsert({
-    where: { email: 'alice@prisma.io' },
+    where: { email: 'test@prisma.io' },
     update: {},
     create: {
-      email: 'alice@prisma.io',
-      password: 'abobus534',
-      name: 'Alice',
+      email: 'test@prisma.io',
+      password: hashedPassword,
+      name: 'TestUser',
       posts: {
-        create: {
-          title: 'Check out Prisma with Next.js',
-          desc:'desc blablabla',
-          content: 'https://www.prisma.io/nextjs',
-          img: 'application.jpg',
-        },
+      create: {
+        title: 'title',
+        desc:'description',
+        content: 'content',
+        img: '/application.jpg',
+      },
       },
     },
   })
   console.log({ alice })
 }
 /*main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1) 
-  })*/
+.then(async () => {
+await prisma.$disconnect()
+})
+.catch(async (e) => {
+console.error(e)
+await prisma.$disconnect()
+process.exit(1)
+})*/
